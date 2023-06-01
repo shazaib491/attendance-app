05a02261bdbfbcf2040edd1be2cf0f567e0f0e13
shazaib491 <shazaibrehman127@gmail.com>
2023-06-01 20:48:15 +0530

    previous url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  // 
-  apiUrl: 'https://4e6d-2401-4900-1c09-e47d-5bb8-70ec-f7ec-877c.in.ngrok.io',
+  apiUrl: 'https://delicate-funny-stage.glitch.me',
 };
-
-// https://delicate-funny-stage.glitch.me
