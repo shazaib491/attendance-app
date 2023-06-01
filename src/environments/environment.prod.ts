18540ddc9264113125b770fe19835c2a84335ab8
shazaib491 <shazaibrehman127@gmail.com>
2023-06-01 20:25:03 +0530

    latest url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://delicate-funny-stage.glitch.me',
+  // 
+  apiUrl: 'https://4e6d-2401-4900-1c09-e47d-5bb8-70ec-f7ec-877c.in.ngrok.io',
 };
+
+// https://delicate-funny-stage.glitch.me
