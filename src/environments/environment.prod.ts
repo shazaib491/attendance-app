32d093590a81d07132ba7caa10ec363bafa21595
shazaib491 <shazaibrehman127@gmail.com>
2023-06-03 20:20:06 +0530

    last commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://delicate-funny-stage.glitch.me',
+  // apiUrl: 'https://delicate-funny-stage.glitch.me',
+  apiUrl:'https://myapp-attendance.onrender.com'
 };
